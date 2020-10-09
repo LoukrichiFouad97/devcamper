8cb2820564129ff59489e7d2cacb99c28b7f1860
fouad97 <loukrichi21@gmail.com>
2020-10-09 16:27:46 +0100

    added user CRUD functionality only for admin

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -11,3 +11,49 @@ export const getUsers = asyncHandler(async (req, res, next) => {
 		users,
 	});
 });
+
+export const getUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.params.userId);
+	if (!user)
+		return next(
+			new ErrorResponse(`User with ID ${req.params.userId} no registered`, 404)
+		);
+
+	res.status(200).json({
+		success: true,
+		user,
+	});
+});
+
+export const createUser = asyncHandler(async (req, res, next) => {
+	const user = await User.create(req.body);
+	if (!user) return next(new ErrorResponse(`Can't create a new user`, 404));
+
+	res.status(200).json({
+		success: true,
+		createdUser: user,
+	});
+});
+
+export const updateUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+		new: true,
+		runValidators: true,
+	});
+	if (!user) return next(new ErrorResponse(`couldn't update user`, 400));
+
+	res.status(200).json({
+		success: true,
+		updatedUser: user,
+	});
+});
+
+export const deleteUser = asyncHandler(async (req, res, next) => {
+	const user = await User.findByIdAndRemove(req.params.userId);
+	if (!user) return next(new ErrorResponse(`can't remove user`, 400));
+
+	res.status(200).json({
+		success: true,
+		removedUser: user,
+	});
+});
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,28 @@
 import express from "express";
 
-import { getUsers } from "../controllers/user.controller";
+import * as userController from "../controllers/user.controller";
+import { requireSignin } from "../middlewares/requireSignin";
+import { hasAuthorization } from "../middlewares/hasAuthorization";
 
 export const userRoute = express.Router();
 
-userRoute.route("/").get(getUsers);
+// Require sign in and Check admin middlwares
+userRoute.use(requireSignin);
+userRoute.use(hasAuthorization("admin"));
 
+// @desc    get all users. and creates a new user
+// @route   GET    /api/v1/users
+// @route   POST   /api/v1/users
+// @access  Private/Admin
+userRoute.route("/").get(userController.getUsers).post(userController.createUser);
 
+// @desc    Read, Update and Delete Users (Admin)
+// @route   GET    /api/v1/users/:userId
+// @route   PUT    /api/v1/users/:userId
+// @route   DELETE /api/v1/users/:userId
+// @access  Private/Admin
+userRoute
+	.route("/:userId")
+	.get(userController.getUser)
+	.put(userController.updateUser)
+	.delete(userController.deleteUser);
