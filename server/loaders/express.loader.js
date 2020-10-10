7e7693c3b27711873af9867b2287108d1ca53e2c
fouad97 <loukrichi21@gmail.com>
2020-10-10 17:58:05 +0100

    added security and cors

--- server/loaders/express.loader.js
+++ server/loaders/express.loader.js
@@ -1,22 +1,32 @@
 import express from "express";
 import compression from "compression";
-import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import helmet from "helmet";
 import mongoSanitizer from "express-mongo-sanitize";
+import limiter from "express-rate-limit";
+import xss from "xss-clean";
+import hpp from "hpp";
+import cors from "cors";
 import path from "path";
 
 export default (app) => {
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	app.use(compression());
-	app.use(helmet());
 	app.use(fileUpload());
 	app.use(express.static(path.join(__dirname, "public")));
 	app.use(cookieParser());
+	app.use(cors());
+	app.use(helmet());
+	app.use(xss());
+	app.use(hpp());
+	app.use(mongoSanitizer());
 	app.use(
-		mongoSanitizer({
-			replaceWith: "*",
+		limiter({
+			windowMs: 10 * 60 * 1000,
+			max: 100,
+			message: "Max Number of requests exceeded, Please try again later",
 		})
 	);
 };
